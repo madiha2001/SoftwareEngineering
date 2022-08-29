da4b4102fd9accb1c522c714d729bb3a0a829562
Akshat Savla <akshat000822@gmail.com>
2022-08-29 09:23:58 -0400

    Updated CONTRIBUTING and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='SoftwareEngineering',
+   version='1.0',
+   description='HW 1',
+   author=['Akshat Savla', 'Tilak Satra', 'Mohit Soni', 'Madiha Mansoo', 'Anagha Patil'],
+   author_email=['asavla@ncsu.edu', 'tsatra@ncsu.edu', 'msoni@ncsu.edu', 'mmansoo@ncsu.edu', 'apatil28@ncsu.edu'],
+   packages=['SoftwareEngineering'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
